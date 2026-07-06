696b7a5a278ed54c6faeb4c2c00e6fdda00b2636
Harshit Singh <harshitsingh1572@gmail.com>
2026-07-06 17:15:08 +0530

    Add files via upload

---
+++ DSA_Project/Design Patterns and Principals/Implementing the Singleton Pattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SingletonPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger1 = Logger.GetInstance();
+            Logger logger2 = Logger.GetInstance();
+
+            logger1.Log("Application Started");
+            logger2.Log("User Logged In");
+
+            Console.WriteLine();
+
+            if (logger1 == logger2)
+            {
+                Console.WriteLine("Only one Logger instance exists.");
+            }
+            else
+            {
+                Console.WriteLine("Multiple instances created.");
+            }
+        }
+    }
+}
